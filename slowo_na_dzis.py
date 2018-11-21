ce44b53d6a65fc58f529236fbfe92b8f357c48b9
Jakub Koralewski <jakub.koralewski1@gmail.com>
2018-11-21 17:20:53 +0100

    fix dublowania tytulu w opisie

--- slowo_na_dzis.py
+++ slowo_na_dzis.py
@@ -6,13 +6,31 @@ with open('slowo_na_dzis.txt', 'r') as plik:
 
 slowo_na_dzis = random.choice(slowo_na_dzis)
 
+tytul = slowo_na_dzis["tytul"]
+opis = slowo_na_dzis["opis"]
+
+
+def unormalizuj_wielkosc(text: str) -> str:
+    text = text.lower()
+    text = text[0].upper() + text[1:]
+    return text
+
+
+# Jesli nie ma tytulowego slowo na poczatku opisu.
+if tytul not in opis[:len(tytul)+2]:
+    # Dodaj je.
+    tytul = unormalizuj_wielkosc(tytul)
+    cytat = f'{tytul} {opis}'
+else:
+    cytat = slowo_na_dzis
+
+
 print(slowo_na_dzis)
 
 
 slowo_na_dzis = {
-    'cytat': f'{slowo_na_dzis["tytul"]} {slowo_na_dzis["opis"]}',
+    'cytat': cytat,
     'autor': slowo_na_dzis["autor"],
-    'tytul': slowo_na_dzis["tytul"],
-    'ksiega': '',
+    'tytul': tytul,
     'z': 'slowo_na_dzis'
 }
