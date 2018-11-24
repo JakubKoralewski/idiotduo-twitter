29115e2c0fa49e751f84705f7c94cb8c662e4844
Jakub Koralewski <jakub.koralewski1@gmail.com>
2018-11-24 23:08:14 +0100

    notacja_wegierska, przygotowanie do wpisania w tweet.py wlasnego cytatu

--- obrazek.py
+++ obrazek.py
@@ -17,7 +17,7 @@ import PIL.ImageFont as ImageFont
 import textwrap
 
 
-def narysujObrys(text, x, y, outlineSize, font):
+def narysuj_obrys(text, x, y, outlineSize, font, draw):
     draw.text((x-outlineSize, y-outlineSize), text, font=font, fill="black")
     draw.text((x+outlineSize, y-outlineSize), text, font=font, fill="black")
     draw.text((x+outlineSize, y+outlineSize), text, font=font, fill="black")
@@ -28,11 +28,68 @@ def narysujObrys(text, x, y, outlineSize, font):
     draw.text((x, y-outlineSize), text, font=font, fill="black")
 
 
-def sumaWysokosc(lista: [], font: ImageFont.truetype) -> int or float:
-    sumaWysokosc = 0
+def suma_wysokosc(lista: [], font: ImageFont.truetype) -> int or float:
+    suma_wysokosc = 0
     for item in lista:
-        sumaWysokosc += font.getsize(item)[1]
-    return sumaWysokosc
+        suma_wysokosc += font.getsize(item)[1]
+    return suma_wysokosc
+
+def znajdz_najdluzszy_cytat(lista: [str], font: ImageFont.truetype) -> str:
+    """Znajduje najdluzszy tekst w dostarczonej liscie."""
+    najdluzszy = lista[0]
+    for line in lista:
+        if font.getsize(line)[0] > font.getsize(najdluzszy)[0]:
+            najdluzszy = line
+    return najdluzszy
+
+obecna_szerokosc_linii = 40 # startowa szerokosc
+imgFraction = 0.85
+VIDEO_RATIO = 1920/1080  # =~ 1.8
+ratio_sensitivity = 0.4
+def wielkosc_czcionki(**kwargs) -> int:
+    """
+    Oblicz potrzebna wielkosc czcionki.
+
+    Args:
+        **kwargs:
+            cytat (str): tekst do ktorego dopasowac czcionke
+            font (ImageFont.truetype): czcionka
+            
+            szerokosc_wideo: img.width
+            wysokosc_wideo: img.height
+            obecna_szerokosc_linii: OPTIONAL (40)
+
+    """
+    
+    font = kwargs["font"]
+    szerokosc_wideo = kwargs["szerokosc_wideo"]
+    wysokosc_wideo = kwargs["wysokosc_wideo"]
+    obecna_szerokosc_linii = kwargs.get("obecna_szerokosc_linii", 40) # default 40, optional
+    cytat_lista = cytat_lista = textwrap.wrap(kwargs["cytat"], width=obecna_szerokosc_linii, fix_sentence_endings=True)
+
+    ratio_szerokosc = imgFraction * szerokosc_wideo
+    ratio_wysokosc = imgFraction * wysokosc_wideo
+
+    najdluzszyCytat = znajdz_najdluzszy_cytat(cytat_lista, font)
+    szerokosc_najdluzszej = font.getsize(najdluzszyCytat)[0]
+    wysokosc = suma_wysokosc(cytat_lista, font)
+    font_size = 1
+
+    while szerokosc_najdluzszej < ratio_szerokosc and wysokosc < ratio_wysokosc:
+        szerokosc_najdluzszej = font.getsize(najdluzszyCytat)[0]
+        wysokosc = suma_wysokosc(cytat_lista, font)
+        # iterate until the text size is just larger than the criteria
+        font_size += 1
+        font = ImageFont.truetype("comic/comic.ttf", font_size)
+
+    if not VIDEO_RATIO - ratio_sensitivity < szerokosc_najdluzszej / wysokosc:
+        cytat = kwargs["cytat"]
+        obecna_szerokosc_linii += 5
+        cytat_lista = textwrap.wrap(
+            cytat, width=obecna_szerokosc_linii, fix_sentence_endings=True)
+        font_size = wielkosc_czcionki()
+
+    return font_size
 
 
 # testowy cytat
@@ -41,100 +98,71 @@ def sumaWysokosc(lista: [], font: ImageFont.truetype) -> int or float:
     'autor': 'Jakub Koralewski'
 } """
 
+def zapisz_obrazek(**kwargs):
+    """
+    Zapisuje obrazek do pliku.
 
-print(slownik_z_cytatem)
-cytat = slownik_z_cytatem['cytat']
-autor = slownik_z_cytatem['autor']
+    Args:
+        **kwargs: 
+            cytat (str): nadpisz cytat tekstu
 
-try:
-    img = Image.open('klatka.jpg')
-except FileNotFoundError:
-    # randomowa_klatka.py fail
-    # sprobuj ponownie
-    import randomowa_klatka
-    img = Image.open('klatka.jpg')
-width, height = img.size
-draw = ImageDraw.Draw(img)
+    """
+    if 'cytat' in **kwargs:
+        cytat = kwargs["cytat"]
+        autor = ''
+        print('cytat overriden\n',cytat, autor)
+    else:
+        print(slownik_z_cytatem)
+        cytat = slownik_z_cytatem['cytat']
+        autor = slownik_z_cytatem['autor']
 
-print(f'cytat: {cytat}')
+    try:
+        img = Image.open('klatka.jpg')
+    except FileNotFoundError:
+        # randomowa_klatka.py fail
+        # sprobuj ponownie
+        import randomowa_klatka
+        img = Image.open('klatka.jpg')
+    
+    width, height = img.size
+    draw = ImageDraw.Draw(img)
+    print(f'cytat: {cytat}')
 
-# portion of image width you want text width to be
-imgFraction = 0.85
+    # portion of image width you want text width to be
 
-font = ImageFont.truetype("comic/comic.ttf", 1)
+    font = ImageFont.truetype("comic/comic.ttf", 1)
 
-cytatLista = textwrap.wrap(cytat, width=40, fix_sentence_endings=True)
+    font_size = wielkosc_czcionki(cytat=cytat, font=font, )
 
+    randColor = tuple([random.randint(100, 255) for i in range(3)])
 
-def znajd_najdluzszy_cytat(lista: [str]) -> str:
-    """Znajduje najdluzszy tekst w dostarczonej liscie."""
-    najdluzszy = lista[0]
-    for line in lista:
-        if font.getsize(line)[0] > font.getsize(najdluzszy)[0]:
-            najdluzszy = line
-    return najdluzszy
+    # zaczynamy pisanie
 
+    # wysrodkuj na osi y
+    cala_wysokosc = suma_wysokosc(cytat_lista, font)
+    srodkowa_wysokosc = height/2 - cala_wysokosc/2
 
-ratio_szerokosc = imgFraction * img.width
-ratio_wysokosc = imgFraction * img.height
-VIDEO_RATIO = 1920/1080  # =~ 1.8
-ratio_sensitivity = 0.4
-obecna_szerokosc_linii = 40
+    print(f'img height: {img.height}, img.width: {img.width}, font: {font.size}, cala_wysokosc: {cala_wysokosc}, srodkowa_wysokosc: {srodkowa_wysokosc}')
 
+    # pisz cytat
+    offset = srodkowa_wysokosc
+    outlineSize = 3
+    for line in textwrap.wrap(cytat, width=40):
 
-def wielkosc_czcionki():
-    global cytatLista
-    global font
-    global obecna_szerokosc_linii
-    najdluzszyCytat = znajd_najdluzszy_cytat(cytatLista)
-    szerokosc_najdluzszej = font.getsize(najdluzszyCytat)[0]
-    wysokosc = sumaWysokosc(cytatLista, font)
-    fontSize = 1
+        # w - szerokosc danej linii tekstu
+        # h - wysokosc danej linii tekstu
+        w, h = draw.textsize(line, font)
 
-    while szerokosc_najdluzszej < ratio_szerokosc and wysokosc < ratio_wysokosc:
-        szerokosc_najdluzszej = font.getsize(najdluzszyCytat)[0]
-        wysokosc = sumaWysokosc(cytatLista, font)
-        # iterate until the text size is just larger than the criteria
-        fontSize += 1
-        font = ImageFont.truetype("comic/comic.ttf", fontSize)
+        x = width/2 - w/2
+        y = offset
 
-    if not VIDEO_RATIO - ratio_sensitivity < szerokosc_najdluzszej / wysokosc:
-        obecna_szerokosc_linii += 5
-        cytatLista = textwrap.wrap(
-            cytat, width=obecna_szerokosc_linii, fix_sentence_endings=True)
-        fontSize = wielkosc_czcionki()
+        # rysujemy obrys
+        narysuj_obrys(line, x, y, outlineSize, font, draw)
 
-    return fontSize
+        draw.text((x, y), line, font=font, fill=randColor)
+        offset += font.getsize(line)[1]
 
+    img.save('klatka_ready.jpg')
 
-fontSize = wielkosc_czcionki()
-
-randColor = tuple([random.randint(100, 255) for i in range(3)])
-
-# zaczynamy pisanie
-
-# wysrodkuj na osi y
-calaWysokosc = sumaWysokosc(cytatLista, font)
-srodkowaWysokosc = height/2 - calaWysokosc/2
-
-print(f'img height: {img.height}, img.width: {img.width}, font: {font.size}, calaWysokosc: {calaWysokosc}, srodkowaWysokosc: {srodkowaWysokosc}')
-
-# pisz cytat
-offset = srodkowaWysokosc
-outlineSize = 3
-for line in textwrap.wrap(cytat, width=40):
-
-    # w - szerokosc danej linii tekstu
-    # h - wysokosc danej linii tekstu
-    w, h = draw.textsize(line, font)
-
-    x = width/2 - w/2
-    y = offset
-
-    # rysujemy obrys
-    narysujObrys(line, x, y, outlineSize, font)
-
-    draw.text((x, y), line, font=font, fill=randColor)
-    offset += font.getsize(line)[1]
-
-img.save('klatka_ready.jpg')
+if __name__ == "__main__":
+    print('imported obrazek.py')
