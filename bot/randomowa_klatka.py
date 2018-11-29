930d0a9b10b040c56bb283a07105eadd251be3e3
Jakub Koralewski <jakub.koralewski1@gmail.com>
2018-11-29 20:42:58 +0100

    CircleCI ffmpeg.exe nie bedzie dzialac na linuxie :)))

--- bot/randomowa_klatka.py
+++ bot/randomowa_klatka.py
@@ -30,13 +30,18 @@ def zapisz_klatke():
     user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
     inputs = '-y -user_agent {user_agent}'
 
-    from bot.config import FFMPEG_PATH
-
-    ff = FFmpeg(
-        executable=FFMPEG_PATH,
-        inputs={urlPliku: inputs},
-        outputs={'output\\klatka.jpg': f'-ss {losowyCzas:.2f} -frames:v 1 -q:v 2'}
-    )
+    if not os.environ['ffmpeg']:
+        from bot.config import FFMPEG_PATH
+        ff = FFmpeg(
+            executable=FFMPEG_PATH,
+            inputs={urlPliku: inputs},
+            outputs={'output\\klatka.jpg': f'-ss {losowyCzas:.2f} -frames:v 1 -q:v 2'}
+        )
+    else:
+        ff = FFmpeg(
+            inputs={urlPliku: inputs},
+            outputs={'output\\klatka.jpg': f'-ss {losowyCzas:.2f} -frames:v 1 -q:v 2'}
+        )
 
     print(ff.cmd)
     ff.run()
