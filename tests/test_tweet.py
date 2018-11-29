902b4e6e0a262a61d135740c798f9d9304c321d4
Jakub Koralewski <jakub.koralewski1@gmail.com>
2018-11-29 19:00:25 +0100

    usuniecie nieudanej proby timeoutu w test_tweet

--- tests/test_tweet.py
+++ tests/test_tweet.py
@@ -1,12 +1,11 @@
 #import pytest
 from bot.tweet import main
-from timeout import timeout
 
-def test_generalny():
+def test_10_klatka():
     for i in range(10):
         """
         Do zrobienia:
         
         Sprawdź czy wykonanie funkcji przekracza 2 minuty, jeśli tak to rzuć błąd.
         """
-        main(test=True, nazwa=f'tests\\output\\klatka_test_ready{i}.jpg')
+        main(test=True, nazwa=f'tests\\output\\klatka_test_ready.jpg')
