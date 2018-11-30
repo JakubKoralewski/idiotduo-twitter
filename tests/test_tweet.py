17f44191805d6c0093ff83230af82993aaf6c05e
Jakub Koralewski <jakub.koralewski1@gmail.com>
2018-11-30 21:38:33 +0100

    bardziej system-tolerant os.path.join w test

--- tests/test_tweet.py
+++ tests/test_tweet.py
@@ -1,5 +1,6 @@
 #import pytest
 from bot.tweet import main
+import os
 
 def test_10_klatka():
     for i in range(10):
@@ -8,4 +9,4 @@ def test_10_klatka():
         
         Sprawdź czy wykonanie funkcji przekracza 2 minuty, jeśli tak to rzuć błąd.
         """
-        main(test=True, nazwa=f'tests\\output\\klatka_test_ready.jpg')
+        main(test=True, nazwa=os.path.join('tests','output', f'klatka_test_ready{i}.jpg')
