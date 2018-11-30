75f92d6e1cfb5d893e70f430c83f08937206033a
Jakub Koralewski <jakub.koralewski1@gmail.com>
2018-11-30 20:07:54 +0100

    bardziej tolerancyjny os.path.join part2

--- bot/config.py
+++ bot/config.py
@@ -1,3 +1,5 @@
-COMIC_SANS_PATH = "static\\comic_sans\\"
-WEB_DRIVER_PATH = 'static\\chromedriver.exe'
-FFMPEG_PATH = 'static\\ffmpeg.exe'
+import os
+
+COMIC_SANS_PATH = os.path.join("static","comic_sans","")
+WEB_DRIVER_PATH = os.path.join('static','chromedriver.exe')
+FFMPEG_PATH = os.path.join('static','ffmpeg.exe')
