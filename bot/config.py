c4d1c05a94fdd616a01cb8a38d5412c70b405894
Jakub Koralewski <jakub.koralewski1@gmail.com>
2019-07-24 14:54:44 +0200

    Reflect the .txt -> .json change in config.py

--- bot/config.py
+++ bot/config.py
@@ -3,4 +3,4 @@ import os
 COMIC_SANS_PATH = os.path.join('static','comic_sans','')
 WEB_DRIVER_PATH = os.path.join('static','chromedriver.exe')
 FFMPEG_PATH = os.path.join('static','ffmpeg.exe')
-SLOWO_NA_DZIS_PATH = os.path.join('static', 'slowo_na_dzis.txt')
+SLOWO_NA_DZIS_PATH = os.path.join('static', 'slowo_na_dzis.json')
